6cf29e6bd8658ba3020c37b15758a15868bcae47
Nikola Tanković <714889+ntankovic@users.noreply.github.com>
2023-11-06 16:55:27 +0100

    Update emailjs settings

--- src/components/BN_ContactUs.vue
+++ src/components/BN_ContactUs.vue
@@ -26,22 +26,27 @@
 
 				<BN_AreaInput class="mt-6 sm:mt-4" label="Description" :max="2000" />
 
-				<button
-					@click="onSubmit()"
+				<button @click="onSubmit()"
 					:class="submitting || type != 'none' ? 'opacity-0 delay-0 pointer-events-none cursor-default' : 'opacity-100'"
 					class="h-12 w-full sm:w-52 | rounded-xl text | font-bold text-BN_PRIMARY hover:text-BN_FONT_WHITE | flex justify-center items-center mt-12 sm:mt-8 z-10 overflow-hidden border-2 group | hover:h-12 hover:w-full sm:hover:w-52 hover:border-0 relative border-BN_PRIMARY hover:border-opacity-0 transition-all duration-500">
-					<div class="absolute h-0 w-0 opacity-0 bg-BN_PRIMARY -z-[1] rounded-2xl group-hover:h-12 group-hover:w-full sm:group-hover:w-52 group-hover:opacity-100 transition-all duration-500"></div>
+					<div
+						class="absolute h-0 w-0 opacity-0 bg-BN_PRIMARY -z-[1] rounded-2xl group-hover:h-12 group-hover:w-full sm:group-hover:w-52 group-hover:opacity-100 transition-all duration-500">
+					</div>
 					Submit
-					<i class="fa-solid fa-paper-plane ml-2 text-BN_PRIMARY group-hover:text-BN_FONT_WHITE group-hover:ml-4 transition-all duration-500"></i>
+					<i
+						class="fa-solid fa-paper-plane ml-2 text-BN_PRIMARY group-hover:text-BN_FONT_WHITE group-hover:ml-4 transition-all duration-500"></i>
 				</button>
 
-				<img class="absolute | animate-spin_slow transition-all duration-500" :src="macroquiet_logo_icon" :class="submitting ? 'h-16 opacity-100 delay-500' : 'h-0 opacity-0 delay-0'" />
+				<img class="absolute | animate-spin_slow transition-all duration-500" :src="macroquiet_logo_icon"
+					:class="submitting ? 'h-16 opacity-100 delay-500' : 'h-0 opacity-0 delay-0'" />
 
-				<BN_alert :show="type == 'warning'" color="rgb(220, 38, 68)" icon="fa-solid fa-triangle-exclamation" class="absolute -mt-12">
+				<BN_alert :show="type == 'warning'" color="rgb(220, 38, 68)" icon="fa-solid fa-triangle-exclamation"
+					class="absolute -mt-12">
 					<b>Error sending message!</b> <br />
 					This is not your fault, we are probably working on it!
 				</BN_alert>
-				<BN_alert :show="type == 'success'" color="rgb(12, 173, 134)" icon="fa-solid fa-circle-check" class="absolute -mt-12">
+				<BN_alert :show="type == 'success'" color="rgb(12, 173, 134)" icon="fa-solid fa-circle-check"
+					class="absolute -mt-12">
 					<b>Your message has been successfully sent!</b> <br />
 					We will contact you very soon!
 				</BN_alert>
@@ -107,17 +112,17 @@ export default {
 				this.submitting = true;
 				try {
 					await emailjs.send(
-						"service_cu0q1tj",
-						"template_gxti4ir",
+						"service_a72hdo2jh",
+						"template_an8qv84",
 						{
-							from_name: values["First Name"] + values["Last Name"],
+							from_name: values["First Name"] + " " + values["Last Name"],
 							email_id: values["Email"],
 							company: values["Company"],
 							phone: values["Phone"],
 							budget: values["Budget"],
 							message: values["Description"],
 						},
-						"Hvy7VtUPqB2M4ZsqU"
+						"xBqJxeH1ONNosl9J5"
 					);
 					await wait(2);
 					this.submitting = false;
